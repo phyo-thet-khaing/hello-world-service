54217a75ca9c00ad87c92ed9d48527f0f3be761a
phyo-thet-khaing <phyothetkhing2002@gmail.com>
2026-06-22 14:27:08 +0630

    change 16 jenkins

--- src/main/java/com/ptk/demo/controller/HelloWorldServiceController.java
+++ src/main/java/com/ptk/demo/controller/HelloWorldServiceController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldServiceController {
 	
-	@GetMapping("/home")
+	@GetMapping("/helloworld")
 	public String index()
 	{
 		return "helloworld-service";
